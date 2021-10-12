177aba82ff49f949b5ee8dfecce56c1118ff980b
Tonis <tonis.anton18@gmail.com>
2021-10-12 18:09:04 +0300

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# javascript-router
-Small and simple JavaScript router
+# JS router
+
+
+## How to use 
+
+import to project, for example
+
+`import Router from '/libraries/router.js';`
+
+#### Change route 
+
+`Router.change('/post/:id');`
+
+
+#### Define routes
+
+```
+Router.on('/project', (event) => {
+    document.body.replaceWith(newElement)
+});
+
+```
+
+** Using * wildcard route**
+
+Wildcard * accepts any route pattern
+
+```
+Router.on('/user/*/posts', (event) => {
+    console.log(event.path[1])  
+});
+```
+
+** Using parameter :value **
+
+Using :value, retrives the value as parameter when parsing route
+
+```
+Router.on('/post/:id', (event) => {
+    console.log(event.parameters.id)  
+});
+```
+
+#### Parameters
+
+Event parameters send along with route change event
+
+* parameters
+* path
+* query
---
+++ main.js
@@ -0,0 +1,56 @@
+class Router {
+    constructor() {
+        this.routes = new Map();
+        this.current = [];
+        window.onpopstate = this.routeUpdated.bind(this);
+    }
+
+    get path() {
+        return window.location.pathname.split('/').filter((x) => x != '');
+    }
+
+    routeUpdated() {
+        const path = this.path;
+
+        if (path.length == 0) {
+            this.routes.get('/')(path);
+            return;
+        }
+
+        // Its' same route return
+        if (this.current.join() === path.join()) return;
+        this.current = path;
+        
+        let parameters = {};
+
+        for (let [route, callback] of this.routes) {
+            const routes = route.split('/').filter((x) => x != '');
+            const matches = routes
+                .map((url, index) => {
+                    if (url == '*') return true;
+                    if (url.includes(':')) {
+                        parameters[url.split(':')[1]] = path[index];
+                        return true;
+                    }
+                    if (url == path[index]) return true;
+                    return false;
+                })
+                .filter((x) => x);
+
+            if (matches.length == routes.length && routes.length > 0) {
+                callback({ path, parameters, query: Object.fromEntries(new URLSearchParams(window.location.search)) });
+            }
+        }
+    }
+
+    on(route, callback) {
+        this.routes.set(route, callback);
+    }
+
+    change(route) {
+        window.history.pushState({ action: 'changeRoute' }, null, route);
+        window.dispatchEvent(new Event('popstate'));
+    }
+}
+
+export default new Router();
